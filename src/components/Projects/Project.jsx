c6dcd0af65786a3ff6b335e3f9e5297cbc93b085
KL <karl.w.langer@gmail.com>
2024-05-06 10:38:21 +0930

    Added redirect

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,5 +1,6 @@
 import {Flex, Image, Stack, Title, Text, Button, Grid, Container} from "@mantine/core"
 import {IconCode, IconClick} from "@tabler/icons-react"
+import {motion} from 'framer-motion'
 
 import classes from "./Project.module.css"
 
@@ -8,7 +9,7 @@ function Project(props) {
   const imageClass = `mantine-hidden-from-lg ${classes.imageClass}`;
 
   return (
-    <Flex  mb={20}>
+    <Flex mb={20}>
       {props.left ? (
         <Image
           src={props.imagePath}
@@ -18,27 +19,31 @@ function Project(props) {
           className="mantine-visible-from-lg"
         />
       ) : null}
-      <Container
-        p={18}
-        m={20}
-        radius="md"
-        style={{ background: "#e9f5db", borderRadius: "20px" }}
-      >
-        <Stack m="auto" align="center" w="80%">
-          <Flex w="100%" justify="space-between" style={{ maxWidth: "800px" }}>
-            <Stack justify="flex-end" gap={0}>
-              <Title order={2}>{props.projectTitle}</Title>
-              <Title order={5}>{props.projectSubHeading}</Title>
-            </Stack>
-            <Image
-              h={100}
-              w="auto"
-              mx="50px"
-              mb="4px"
-              src={props.iconPath}
-              fit="contain"
-              className="mantine-visible-from-lg"
-            />
+        <Container
+          p={18}
+          m={20}
+          radius="md"
+          style={{ background: "#e9f5db", borderRadius: "20px" }}
+        >
+          <Stack m="auto" align="center" w="80%">
+            <Flex
+              w="100%"
+              justify="space-between"
+              style={{ maxWidth: "800px" }}
+            >
+              <Stack justify="flex-end" gap={0}>
+                <Title order={2}>{props.projectTitle}</Title>
+                <Title order={5}>{props.projectSubHeading}</Title>
+              </Stack>
+              <Image
+                h={100}
+                w="auto"
+                mx="50px"
+                mb="4px"
+                src={props.iconPath}
+                fit="contain"
+                className="mantine-visible-from-lg"
+              />
               <Image
                 src={props.imagePath}
                 w={200}
@@ -46,50 +51,50 @@ function Project(props) {
                 m="auto"
                 className={imageClass}
               />
-          </Flex>
+            </Flex>
 
-          <Text>{props.projectDescription}</Text>
+            <Text>{props.projectDescription}</Text>
 
-          <a
-            href={props.liveLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            m="2px"
-            style={{ width: "60%" }}
-          >
-            <Button
-              variant="outline"
-              color="#E2725B"
-              rightSection={<IconClick size={18} />}
-              style={{
-                width: "100%",
-              }}
+            <a
+              href={props.liveLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              m="2px"
+              style={{ width: "60%" }}
             >
-              Live Deploy
-            </Button>
-          </a>
+              <Button
+                variant="outline"
+                color="#E2725B"
+                rightSection={<IconClick size={18} />}
+                style={{
+                  width: "100%",
+                }}
+              >
+                Live Deploy
+              </Button>
+            </a>
 
-          <a
-            href={props.repoLink}
-            target="_blank"
-            rel="noopener noreferrer"
-            style={{ width: "60%" }}
-          >
-            <Button
-              color="#E2725B"
-              variant="default"
-              rightSection={<IconCode size={18} />}
-              style={{
-                width: "100%",
-                backgroundColor: "#E2725B",
-                color: "white",
-              }}
+            <a
+              href={props.repoLink}
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ width: "60%" }}
             >
-              The Code
-            </Button>
-          </a>
-        </Stack>
-      </Container>
+              <Button
+                color="#E2725B"
+                variant="default"
+                rightSection={<IconCode size={18} />}
+                style={{
+                  width: "100%",
+                  backgroundColor: "#E2725B",
+                  color: "white",
+                }}
+              >
+                The Code
+              </Button>
+            </a>
+          </Stack>
+        </Container>
       {!props.left ? (
         <Image
           src={props.imagePath}
